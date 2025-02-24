bd64e17ad985f82287bb7b8bc2bf7d730d53e941
Robert Bain <31903812+bainro@users.noreply.github.com>
2025-02-23 20:13:05 -0800

    Update dataset.py

--- lib/dataset.py
+++ lib/dataset.py
@@ -8,7 +8,8 @@ from torch.utils.data import DataLoader, Dataset
 
 class VideoDataset(Dataset):
 
-    def __init__(self, directory, mode='train', frame_sample_rate=1, dim=3):
+    def __init__(self, directory, mode='train', frame_sample_rate=1, dim=3, **kwargs):
+        super().__init__(*args, **kwargs)
         self.short_side = [128, 160]
         self.crop_size = 112
         self.frame_sample_rate = frame_sample_rate
