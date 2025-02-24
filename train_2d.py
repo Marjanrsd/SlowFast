7ce855c474300327d4eafa7852021028414ac474
Robert Bain <31903812+bainro@users.noreply.github.com>
2025-02-23 20:15:45 -0800

    Update train_2d.py

--- train_2d.py
+++ train_2d.py
@@ -25,9 +25,9 @@ train_trans = T.Compose([
 
 image_datasets = {}
 # directory, mode='train', frame_sample_rate=1, dim=3
-image_datasets['train'] = VideoDataset('./', mode='train', dim=3, shuffle=True)
-image_datasets['val'] = VideoDataset('./', mode='test', shuffle=False)
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=256, shuffle=True, 
+image_datasets['train'] = VideoDataset('./', mode='train', dim=3)
+image_datasets['val'] = VideoDataset('./', mode='test')
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=128, shuffle=True, 
                                               num_workers=4, pin_memory=True)
               for x in ['train', 'val']}
 dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
